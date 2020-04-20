bbd114536ed6c7e351b407913b8b9ec585471618
Aditya Sen <33868647+adityasen53@users.noreply.github.com>
2020-04-21 01:58:29 +0530

    uploading the code

---
+++ GET_Request/CreateNewChannel.py
@@ -0,0 +1,54 @@
+import requests
+import json
+import jsonpath
+#1. Create a new Channel & Joining the new channel
+
+CreateChannelURL = "https://slack.com/api/channels.create"
+file = open("C:\\Users\\adsen\\PycharmProjects\\AdityaPython\\json_inputs\\CreateChannel.json","r")
+json_input = file.read()
+requests_json = json.loads(json_input)
+#post request with json input
+response = requests.post(CreateChannelURL,requests_json)
+response_obj = json.loads(response.text)
+id = response_obj["channel"]["id"]
+print(id)
+old_name = response_obj["channel"]["name"]
+print("Channel name before using Rename API: ",old_name)
+
+#2 Join the New Channel
+#Channel gets already joined when a user creates a channel from the same auth-token.
+
+#3. Rename the Channel
+
+RenameURL = "https://slack.com/api/conversations.rename"
+f = open("C:\\Users\\adsen\\PycharmProjects\\AdityaPython\\json_inputs\\RenameChannel.json","r")
+requests_json1 = json.loads(f.read())
+requests_json1["channel"]  = id
+response2 = requests.post(RenameURL,requests_json1)
+response_obj2 = json.loads(response2.text)
+new_name = response_obj2["channel"]["name"]
+print("Channel name after using Rename API: ",new_name)
+
+#List all the channels
+
+ChannelListURL = "https://slack.com/api/channels.list"
+f = open("C:\\Users\\adsen\\PycharmProjects\\AdityaPython\\json_inputs\\RenameChannel.json","r")
+requests_json1 = json.loads(f.read())
+response3 =  requests.get(ChannelListURL,requests_json1)
+channel_list = json.loads(response3.text)
+#for names in channel_list[channels]:
+  # print(names["name"])
+
+#print(type(channel_list)
+
+#Archive the channel
+
+ArchiveChannelURL = "https://slack.com/api/conversations.archive"
+response4 = requests.post(ArchiveChannelURL,requests_json1)
+print("Channel is archived", response4.text)
+response4 = requests.post(ArchiveChannelURL,requests_json1)
+print("Channel is already archived", response4.text)
+
+
+
+
